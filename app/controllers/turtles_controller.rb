04a62df8d9176484de6a2c29d8e7e7cdb37bf321
Gordana Marjanovic <gogamar@gmail.com>
2022-05-24 15:36:17 +0200

    controller with all parameters

--- app/controllers/turtles_controller.rb
+++ app/controllers/turtles_controller.rb
@@ -38,6 +38,6 @@ class TurtlesController < ApplicationController
   private
 
   def turtle_params
-    params.require(:turtle).permit(:first_name, :last_name)
+    params.require(:turtle).permit(:first_name, :last_name, :photo, :description, :price)
   end
 end
