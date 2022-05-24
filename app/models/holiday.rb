4439fd8d536021063305a9d2aa9313b6ff68245e
ryadsari <ryadsari@protonmail.com>
2022-05-24 15:38:16 +0200

    deleted holiday to work a new system with dates

--- app/models/holiday.rb
+++
@@ -1,8 +0,0 @@
-class Holiday < ApplicationRecord
-  belongs_to :user
-  belongs_to :turtle
-
-  validates :user, presence: true,
-  validates :turtle, presence: true,
-  validates :status, presence: true, inclusion: { in: ["accepted", "pending", "rejected"]}
-end
