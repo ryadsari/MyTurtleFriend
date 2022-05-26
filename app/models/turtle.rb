9c4c75253c3bc7b5b3448da6891edbe1e25f3449
Gordana Marjanovic <gogamar@gmail.com>
2022-05-26 16:05:48 +0200

    booking confirmation page

--- app/models/turtle.rb
+++ app/models/turtle.rb
@@ -3,4 +3,5 @@ class Turtle < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   has_one_attached :photo
+  has_many :bookings, dependent: :destroy
 end
