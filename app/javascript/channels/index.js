cf1d3a5537acf7561c0c7956ef85e79f64de2545
ryadsari <ryadsari@protonmail.com>
2022-10-14 17:48:55 +0200

    front-end rework

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,14 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-500px";
+  }
+  prevScrollpos = currentScrollPos;
+}
